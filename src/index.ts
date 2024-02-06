575e02bc9e031ec9fb05b377755152c3791f5d72
Pavlov Kostyantyn <realanka77@gmail.com>
2024-02-06 16:05:05 +0200

    add: change a connect template to my data

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,22 @@
-import mongoose from 'mongoose';
-import { PORT } from './constants';
+import mongoose from "mongoose";
+import { PORT } from "./constants";
 
-import * as serverService from './services/server.service';
-
+import * as serverService from "./services/server.service";
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(
+      "mongodb+srv://kosta4310:<password>@cluster0.ohi9bdu.mongodb.net/"
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
-      console.log('Сервер ожидает подключения...');
-    })
+      console.log("Сервер ожидает подключения...");
+    });
   } catch (error) {
     console.log(error);
   }
 })();
 
-
-
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.disconnect();
   process.exit();
 });
